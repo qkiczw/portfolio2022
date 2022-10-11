4d1997197fea72809f8b516a1dd1ec81ee61f901
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2022-10-11 19:07:24 +0200

    Add swiper.js to the project and add some margin to the project.

--- src/js/script.js
+++ src/js/script.js
@@ -6,3 +6,28 @@ const navbarLinks = document.querySelector(".navbar__links");
 navbarToggleButton.addEventListener("click", function () {
   navbarLinks.classList.toggle("active");
 });
+
+// swiper
+const swiper = new Swiper(".swiper", {
+  // Default parameters
+  slidesPerView: 1,
+  spaceBetween: 10,
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+    // when window width is >= 1200px
+    1200: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+  },
+});
